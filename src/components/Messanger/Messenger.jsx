ea992d9d6c431dba208eba334b12aade7c7577c6
SklifCybe <i.s.toaccept@gmail.com>
2021-02-28 15:04:54 +0300

    Change MyPosts.jsx and Messenger.jsx

--- src/components/Messanger/Messenger.jsx
+++ src/components/Messanger/Messenger.jsx
@@ -16,17 +16,33 @@ const Message = props => {
 };
 
 const Messenger = () => {
+    let dialogsData = [
+        {name: 'Ilya', id: '0'},
+        {name: 'Evgeniy', id: '1'},
+        {name: 'Ilyas', id: '2'},
+        {name: 'Viktor', id: '3'},
+        {name: 'Danik', id: '4'},
+    ];
+
+    let messengesData = [
+        {id: '0', message: 'Hello'},
+        {id: '1', message: 'How are you?'},
+        {id: '2', message: 'Why did you leave me?'},
+    ];
+
     return (
         <div className={classes.messenger}>
             <section className={classes.dialogs}>
-                <Dialog name='Ilya' id='0' />
-                <Dialog name='Evgeniy' id='1' />
-                <Dialog name='Ilyas' id='2' />
-                <Dialog name='Viktor' id='3' />
-                <Dialog name='Danik' id='4' />
+                <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />
+                <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />
+                <Dialog name={dialogsData[2].name} id={dialogsData[2].id} />
+                <Dialog name={dialogsData[3].name} id={dialogsData[3].id} />
+                <Dialog name={dialogsData[4].name} id={dialogsData[4].id} />
             </section>
             <section className='messages'>
-                <Message message='hello' />
+                <Message message={messengesData[0].message} id={messengesData[0].id} />
+                <Message message={messengesData[1].message} id={messengesData[1].id} />
+                <Message message={messengesData[2].message} id={messengesData[2].id} />
             </section>
         </div>
     );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,11 @@ import Post from './Post/Post';
 import classes from './MyPosts.module.css';
 
 const MyPosts = () => {
+    let postsData = [
+        {id: '0', message: 'Hi, how are you?', likeCount: '20'},  
+        {id: '1', message: 'It\'s my second post', likeCount: '15'} 
+    ];
+
     return (
         <section className={classes.myPosts}>
             <h3>My posts:</h3>
@@ -11,8 +16,8 @@ const MyPosts = () => {
             </div>
             
             <ul>
-                <Post message='Hi, how are you?' likeCount='20' />
-                <Post message="It's my second post" likeCount='15' />
+                <Post message={postsData[0].message} likeCount={postsData[0].likeCount} />
+                <Post message={postsData[1].message} likeCount={postsData[1].likeCount} />
             </ul>
         </section>
     );
