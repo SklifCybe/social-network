ef0b9646229893ccdf5eb59662ede675666801bb
SklifCybe <i.s.toaccept@gmail.com>
2021-05-06 21:46:52 +0300

    install axios and small layout users page

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
+import axios from 'axios';
 import styles from './Users.module.css';
+import userPhoto from '../../assets/images/default-image.png';
 
 const Users = (props) => {
+    if (props.users.length === 0) {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            .then(res => {
+                //console.log(res.data.items)
+                props.setUsers(res.data.items);
+            });
+    };
+
     return (
-        <div>
-            {
-                props.users.map(user =>
-                    <div key={user.id}>
-                        <span>
-                            <img src={user.avatarUrl} className={styles.avatar} />
-                            <div className={styles.btnFollow}>
-                                {user.followed
-                                    ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
-                                    : <button onClick={() => props.follow(user.id)}>Follow</button>}
+        <div className={styles.content}>
+            <h2 className={styles.title}>Users</h2>
+            <div className={styles.users}>
+                {
+                    props.users.map(user =>
+                        <div key={user.id}>
+                            <div className={styles.user}>
+                                <img src={user.photos.small != null ? user.photos.small : userPhoto} className={styles.avatar} />
+                                <div className={styles.btnFollow}>
+                                    {user.followed
+                                        ? <button onClick={() => props.unfollow(user.id)}>Unfollow</button>
+                                        : <button onClick={() => props.follow(user.id)}>Follow</button>}
+                                </div>
                             </div>
-                        </span>
-                        <span>
-                            <span>
-                                <div>{user.fullName}</div>
-                                <div>{user.status}</div>
-                            </span>
-                            <span>
-                                <div>{user.location.contry}</div>
-                                <div>{user.location.city}</div>
-                            </span>
-                        </span>
-                    </div>)
-            }
+                            <div className={styles.userInfo}>
+                                <div>
+                                    <div className={styles.fullname}>{user.name}</div>
+                                    <div className={styles.status}>{user.status}</div>
+                                </div>
+                            </div>
+                        </div>)
+                }
+            </div>
         </div>
     );
 };
